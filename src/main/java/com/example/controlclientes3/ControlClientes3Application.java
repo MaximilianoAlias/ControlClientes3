986b4735075c0cde6cc42f654cc48a569a0f5dd5
Maximiliano Alias <damianalias1991@gmail.com>
2024-08-20 19:17:34 -0300

    EJERCICIO DE CONTROL DE CLIENTES 3 - INCLUYE CRUD Y VALIDACIONES DE CAMPOS

---
+++ src/main/java/com/example/controlclientes3/ControlClientes3Application.java
@@ -0,0 +1,13 @@
+package com.example.controlclientes3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlClientes3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlClientes3Application.class, args);
+    }
+
+}
